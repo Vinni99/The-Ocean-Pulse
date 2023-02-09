6c2c648763bca9960d6f71e3cd39c2e8c764374c
Kaneknah <jamesnbaxley@protonmail.com>
2023-02-08 21:26:45 -0700

    Added psudeo code for api routes and renamed it

---
+++ controllers/apiRoutes.js
@@ -0,0 +1,14 @@
+// add routes for
+const db = require("../models");
+
+//accessing the db for the locals
+db.local.create({
+	//adding database with sequalize?
+});
+
+//signup route
+
+//login route
+
+//logout route
+appendFile.get;
