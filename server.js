105efe09a43846bdd75cd5a9d982ad0a79c0e857
Kaneknah <jamesnbaxley@protonmail.com>
2023-02-08 19:23:23 -0700

    added sequelize aspects to server.

--- server.js
+++ server.js
@@ -7,18 +7,26 @@ const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+//setting the use of express
 const app = express();
-const PORT = proecess.env.PORT || 3306;
+const PORT = process.env.PORT || 3306;
 
 // if we need helpers const hbs = exphbs.create({helpers});
 
-//for adding login session
+//for adding login session with Sequelize Store
 const sess = {
 	secret: "Secret Session",
 	cookie: {},
 	resave: false,
 	saveUninitialized: true,
 	store: new SequelizeStore({
-		db: sequalize,
+		db: sequelize,
 	}),
 };
+
+app.use(session(sess));
+
+//running our Express app
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log("Now listening to the PORT"));
+});
