512669a09a3143054cf8c078854c39bf85a7859d
Alt B 2022 <massie.w@gmail.com>
2023-02-09 17:03:39 -0800

    addded second location.  TBH it's not letting me push so I made changes to do another full round of add . commit, push

--- models/locations.js
+++ models/locations.js
@@ -2,13 +2,20 @@
 const router = require('express').Router();
 const surfSpots = [
     {
-     
       surf_spot: 'Ocean Beach',
       type: 'Beach - sand bottom with shifting peaks',
       optimal_swell_direction: 'NW, W, SW',
       optimal_wind: 'light to none - up to 10mph NE',
       optimal_swell_size: 'Waist high to double-overhead (note: can handle much bigger but becomes impossible to paddle out)',
       optimal_tide: 'All-tides',
+    },
+    {
+      surf_spot: 'Mavericks',
+      type: 'Reef - rock bottom',
+      optimal_swell_direction: 'NW, W, SW',
+      optimal_wind: 'light to none - up to 10mph NE',
+      optimal_swell_size: 'Minimum size triple overhead.  Can handle and routinely sees days up to 5-6x overhead',
+      optimal_tide: 'Low to Mid',
     },
    
   ];
