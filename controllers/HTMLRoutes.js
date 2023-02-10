fdfdec3d58fc41ac3904d3e585b409f110d70e9a
Kaneknah <jamesnbaxley@protonmail.com>
2023-02-09 22:29:39 -0700

    Rewrote HTML Routes for correct syntax

--- controllers/HTMLRoutes.js
+++ controllers/HTMLRoutes.js
@@ -3,27 +3,22 @@
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
+	// get route for search locations
+	app.get("/searchLocation", function (req, res) {
+		//if logged in render search locations
+		if (req.session.logged_in) {
+			res.redirect("searchLocation");
+			//if not logged in send to login page
+		} else {
+			res.render("login");
+		}
+	});
+// makes the landing page home page
 	app.get("/", function (req, res) {
-		if (req.user) {
-			res.redirect("/home");
-		} else {
-			res.render("signup", { js: ["signup.js"] });
-		}
+		res.render("home");
 	});
-
-	app.get("/login", function (req, res) {
-		if (req.user) {
-			res.redirect("/home");
-		} else {
-			res.render("login", { js: ["login.js"] });
-		}
-	});
-
-	app.get("/home", isAuthenticated, function (req, res) {
-		res.render("home", { js: ["home.js"] });
-	});
-
-	app.get("/searchLocals", isAuthenticated, function (req, res) {
-		res.render("searchLocals", { js: ["locations.js"] });
+// checks for authentication for search location page.
+	app.get("/searchLocation", isAuthenticated, function (req, res) {
+		res.render("searchLocation");
 	});
 };
