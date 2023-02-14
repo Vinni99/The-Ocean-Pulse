43a605b24ccf5955cd7782a4104a3f6aff826625
unknown <brhouma85@gmail.com>
2023-02-13 21:25:05 -0800

    HTMLRoutes and mainhandlebars

--- controllers/HTMLRoutes.js
+++ controllers/HTMLRoutes.js
@@ -1,24 +1,30 @@
-// add routes for HTML
-
 var isAuthenticated = require("../middleware/isAuthenticated");
-
-module.exports = function (app) {
-	// get route for search locations
-	app.get("/searchLocation", function (req, res) {
-		//if logged in render search locations
-		if (req.session.logged_in) {
-			res.redirect("searchLocation");
-			//if not logged in send to login page
-		} else {
-			res.render("login");
-		}
-	});
+const router = require("express").Router();
+// module.exports = function (router) {
+    // get route for search locations
+    router.get("/searchLocation", function (req, res) {
+        //if logged in render search locations
+        if (req.session.logged_in) {
+            res.redirect("searchLocation");
+            //if not logged in send to login page
+        } else {
+            res.render("login");
+        }
+    });
 // makes the landing page home page
-	app.get("/", function (req, res) {
-		res.render("home");
-	});
+    router.get("/", function (req, res) {
+        console.log("home page");
+        res.render("home");
+    });
 // checks for authentication for search location page.
-	app.get("/searchLocation", isAuthenticated, function (req, res) {
-		res.render("searchLocation");
-	});
-};
+    router.get("/searchLocation", isAuthenticated, function (req, res) {
+        res.render("searchLocation");
+    });
+    router.get("/login", function (req, res) {
+        res.render("login");
+    });
+    router.get("/signup", function (req, res) {
+        res.render("signup");
+    });
+// };
+module.exports = router;
