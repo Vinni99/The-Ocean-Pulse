0376b524ab5c528c0c130dca91306a16b765756b
Kaneknah <jamesnbaxley@protonmail.com>
2023-02-15 13:28:49 -0700

    fixed app.use(router) location and minor js issues

--- public/js/main.js
+++ public/js/main.js
@@ -1,85 +1,77 @@
-
-
 const homeBtn = document.querySelector("#home-btn").innerHTML;
 const loginBtn = document.querySelector("#login-btn").innerHTML;
 const logoutBtn = document.querySelector("#logout-btn").innerHTML;
 const locationBtn = document.querySelector("#location-btn").innerHTML;
 
-
 // function runHome ()
 // { Handlebars.compile(homeBtn)
 // }
 
- const response = await fetch(`/comment`, {
-    method: 'POST',
-    body: JSON.stringify({
-      dish_name,
-      description,
-      guest_name,
-      has_nuts,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert('Failed to add comment');
-  }
+const response = await fetch(`/comment`, {
+	method: "POST",
+	body: JSON.stringify({
+		dish_name,
+		description,
+		guest_name,
+		has_nuts,
+	}),
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
 
+if (response.ok) {
+	document.location.replace("/");
+} else {
+	alert("Failed to add comment");
+}
 
-
+document.getElementById("#home-button").addEventListener("click", async (e) => {
+	try {
+		let response = await fetch("/home");
+		let data = await response.json();
+		// document.getElementById("home").innerHTML = data.home;
+	} catch (e) {
+		console.log(e);
+		alert("Request for home page failed.");
+	}
+});
 
 document
-	.getElementById("#home-button")
+	.getElementById("#login-button")
 	.addEventListener("click", async (e) => {
 		try {
-			let response = await fetch("/home");
+			let response = await fetch("/login");
 			let data = await response.json();
 			// document.getElementById("home").innerHTML = data.home;
 		} catch (e) {
 			console.log(e);
-			alert("Request for home page failed.");
+			alert("Request for login page failed.");
 		}
 	});
 
-    document
-			.getElementById("#login-button")
-			.addEventListener("click", async (e) => {
-				try {
-					let response = await fetch("/login");
-					let data = await response.json();
-					// document.getElementById("home").innerHTML = data.home;
-				} catch (e) {
-					console.log(e);
-					alert("Request for login page failed.");
-				}
-			});
+document
+	.getElementById("#logout-button")
+	.addEventListener("click", async (e) => {
+		try {
+			let response = await fetch("/logout");
+			let data = await response.json();
+			// document.getElementById("home").innerHTML = data.home;
+		} catch (e) {
+			console.log(e);
+			alert("Request for logout page failed.");
+		}
+	});
 
-  document
-		.getElementById("#logout-button")
-		.addEventListener("click", async (e) => {
-			try {
-				let response = await fetch("/logout");
-				let data = await response.json();
-				// document.getElementById("home").innerHTML = data.home;
-			} catch (e) {
-				console.log(e);
-				alert("Request for logout page failed.");
-			}
-		});  
-        
-  document
-		.getElementById("#locations-button")
-		.addEventListener("click", async (e) => {
-			try {
-				let response = await fetch("/locations");
-				let data = await response.json();
-				// document.getElementById("home").innerHTML = data.home;
-			} catch (e) {
-				console.log(e);
-				alert("Request for locations page failed.");
-			}
-		});       
+document
+	.getElementById("#locations-button")
+	.addEventListener("click", async (e) => {
+		try {
+			let response = await fetch("/locations");
+			let data = await response.json();
+			// document.getElementById("home").innerHTML = data.home;
+		} catch (e) {
+			console.log(e);
+			alert("Request for locations page failed.");
+		}
+	});
