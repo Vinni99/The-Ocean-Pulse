75986894cf65411848adcf45998bb85acd20bd69
Kaneknah <jamesnbaxley@protonmail.com>
2023-02-15 13:12:05 -0700

    updated Seeds data to allow for user and location rendering

--- seeds/locationData.js
+++ seeds/locationData.js
@@ -1,4 +1,3 @@
-const router = require("express").Router();
 const surfSpots = [
 	{
 		surf_spot: "Ocean Beach",
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,18 +1,11 @@
 const sequelize = require("../config/connections");
 const { User, Locations } = require("../models");
 
-const userData = require("./userData.json");
+const userData = require("./userData.js");
 const surfSpots = require("./locationData.js");
 
 const seedDatabase = async () => {
 	await sequelize.sync({ force: true });
-
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-console.log('\n----- DATABASE SEEDED -----\n')
-  process.exit(0);
 
 	await User.bulkCreate(userData, {
 		individualHooks: true,
@@ -22,7 +15,7 @@ console.log('\n----- DATABASE SEEDED -----\n')
 		individualHooks: true,
 		returning: true,
 	});
-	console.log("user creted");
+
 	process.exit(0);
 };
 
