6af7ae5ae4bf6f8d301b9fcde02fdf71716675cc
Alt B 2022 <massie.w@gmail.com>
2023-02-18 14:04:39 -0800

    Fixed syntax to locations.js eventListener

--- public/js/locations.js
+++ public/js/locations.js
@@ -2,7 +2,6 @@ const addLocationFormHandler = async (event) => {
 	event.preventDefault();
 	document.location.replace("/addlocation");
 
+}
 
-document
-	.querySelector("location-btn")
-	.addEventListener("submit", addLocationFormHandler);
+document.querySelector("location-btn").addEventListener("submit", addLocationFormHandler);
