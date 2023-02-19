3e5010b81f143b52af6f9c49158fca0599419512
unknown <brhouma85@gmail.com>
2023-02-18 16:25:51 -0800

    changes

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,19 +5,21 @@ const signupFormHandler = async (event) => {
 	const email = document.querySelector("#email-signup").value.trim();
 	const password = document.querySelector("#password-signup").value.trim();
 
-	if (name, email && password) {
-		const response = await fetch("/api/users", {
+	if (name && email && password) {
+		const response = await fetch("/api/user/signup", {
 			method: "POST",
-			body: JSON.stringify({name, email, password }),
+			body: JSON.stringify({ name, email, password }),
 			headers: { "Content-Type": "application/json" },
 		});
 
 		if (response.ok) {
-			document.location.replace("/home");
+			document.location.replace("/login");
 		} else {
 			alert(response.statusText);
 		}
 	}
 };
 
-document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
+document
+	.querySelector(".signup-form")
+	.addEventListener("submit", signupFormHandler);
