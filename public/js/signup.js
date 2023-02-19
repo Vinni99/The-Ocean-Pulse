b23b3a9b35af9f484e639dff02c22643b9782bb8
Alt B 2022 <massie.w@gmail.com>
2023-02-19 15:37:05 -0800

    Fixed bug on signUp.handlebars to allow for proper functionality

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,22 +1,4 @@
 const signupFormHandler = async (event) => {
-<<<<<<< HEAD
-    event.preventDefault();
-    const name = document.querySelector("#name-signup").value.trim();
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
-    if (name && email && password) {
-        const response = await fetch("/api/user/signup", {
-            method: "POST",
-            body: JSON.stringify({ name, email, password }),
-            headers: { "Content-Type": "application/json" },
-        });
-        if (response.ok) {
-            document.location.replace("/login");
-        } else {
-            alert(response.statusText);
-        }
-    }
-=======
 	event.preventDefault();
 
 	const name = document.querySelector("#name-signup").value.trim();
@@ -36,7 +18,6 @@ const signupFormHandler = async (event) => {
 			alert(response.statusText);
 		}
 	}
->>>>>>> f09f7aeca3376a1b9c4516358cc2071ef8bb9578
 };
 document
     .querySelector(".signup-form")
