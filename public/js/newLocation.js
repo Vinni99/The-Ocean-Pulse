6ae0664f3bf2054b02438a2085d8be43c2d297dc
Alt B 2022 <massie.w@gmail.com>
2023-02-18 18:00:55 -0800

    added remainder of parameters for newLocation.js

--- public/js/newLocation.js
+++ public/js/newLocation.js
@@ -3,4 +3,9 @@ const newFormHandler = async (event) => {
 
     const surfSpot = document.querySelector('#surf-spot-name').value.trim();
     const locationName = document.querySelector('#location-name').value.trim();
+    const beachType = document.querySelector('#beach-type').value.trim();
+    const optimalSwellDirection= document.querySelector('#optimal-swell-direction').value.trim();
+    const optimalWind = document.querySelector('#optimal-wind').value.trim();
+    const optimalSwellSize = document.querySelector('#optimal-swell-size').value.trim();
+    const optimalTide = document.querySelector('#optimal-tide').value.trim();
 }
