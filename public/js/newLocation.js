1fedc23075b4c660170f974f008cf8eb774a019b
Alt B 2022 <massie.w@gmail.com>
2023-02-18 17:59:45 -0800

    began work on newLocation.js newFormHandler

---
+++ public/js/newLocation.js
@@ -0,0 +1,6 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const surfSpot = document.querySelector('#surf-spot-name').value.trim();
+    const locationName = document.querySelector('#location-name').value.trim();
+}
